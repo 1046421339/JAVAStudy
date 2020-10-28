e39b8e1cf146672e9a8499ccd70e3fb83e298511
unknown <1046421339@qq.com>
2020-10-29 00:01:05 +0800

    day09

---
+++ day09/Test3.java
@@ -0,0 +1,16 @@
+package com.jjli.day09;
+
+/**
+ * @ClassName Test3
+ * @Description TODO
+ * @Author Jason
+ * @Date 2020/10/28
+ **/
+public class Test3 {
+    public static void main(String[] args) {
+        //   控制台输出所有布尔值类型常量值
+        System.out.println(true);
+        System.out.println(false);
+    }
+
+}
