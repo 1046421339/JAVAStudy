bfcff17e2ee032954d1221430b0bed30eb5078c4
unknown <1046421339@qq.com>
2020-10-29 00:00:00 +0800

    day08

---
+++ day08/Test2.java
@@ -0,0 +1,25 @@
+package com.jjli.day08;
+
+/**
+ * @ClassName Test2
+ * @Description TODO
+ * @Author Jason
+ * @Date 2020/10/28
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        // 控制台输出任意5行整数类型常量值
+        System.out.println(-2147483648);
+        System.out.println(-100);
+        System.out.println(0);
+        System.out.println(100);
+        System.out.println(2147483647);
+
+        // 控制台输出任意5行小数类型常量值
+        System.out.println(-100.0);
+        System.out.println(-10.0);
+        System.out.println(0.0);
+        System.out.println(10.9);
+        System.out.println(100.9);
+    }
+}
