d982259d8b72acd24580887d7f072e02ef833cab
unknown <1046421339@qq.com>
2020-11-02 23:10:57 +0800

    day14

---
+++ day14/RegexMatches3.java
@@ -0,0 +1,33 @@
+package com.jjli.day14;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @ClassName RegexMatches3
+ * @Description TODO
+ * @Author Jason
+ * @Date 2020/11/2
+ **/
+public class RegexMatches3 {
+    private static final String REGEX = "foo";
+    private static final String INPUT = "foooooooooooooooooo";
+    private static final String INPUT2 = "oooooofoooooooo";
+    private static Pattern pattern;
+    private static Matcher matcher;
+    private static Matcher matcher2;
+
+    public static void main(String[] args) {
+        pattern = Pattern.compile(REGEX);
+        matcher = pattern.matcher(INPUT);
+        matcher2 = pattern.matcher(INPUT2);
+
+        System.out.println("Current REGEX is:" + REGEX);
+        System.out.println("Current INPUT is:" + INPUT);
+        System.out.println("Current INPUT2 is:"+INPUT2);
+
+        System.out.println("lookingAT()：" + matcher.lookingAt());
+        System.out.println("matches():" + matcher.matches());
+        System.out.println("lookingAt():" + matcher2.lookingAt());
+    }
+}
